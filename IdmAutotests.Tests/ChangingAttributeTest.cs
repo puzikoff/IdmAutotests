153380363d3f55c6de1758cd7ba68255fa5f6bcd
puzikoff <chepelve@gmail.com>
2014-10-03 18:22:51 +0400

    ChangingAttribute pageobject

---
+++ IdmAutotests.Tests/ChangingAttributeTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IdmAutotests.WebPages;
+using IdmAutotests.WebPages.Root;
+using IdmAutotests.Utilities;
+using IdmAutotests.Tests.Properties;
+
+namespace IdmAutotests.Tests
+{
+    [TestClass]
+    public class ChangingAttributeTest : TestBase
+    {
+        [TestMethod]
+        public void ChangingLoginFieldSize()
+        {
+            Pages.LogOn.Open();
+            Browser.ExecuteJavaScript("document.getElementById('UserName').setAttribute('class', 'text-box single-line textLage')");
+            Thread.Sleep(1500);
+        }
+    }
+}
