ba97f2f28923c0dbb667861c0a9adeefdec6db66
puzikoff <chepelve@gmail.com>
2014-10-03 17:06:40 +0400

    2

--- IdmAutotests.WebPages/PageBase.cs
+++ IdmAutotests.WebPages/PageBase.cs
@@ -9,7 +9,7 @@ namespace IdmAutotests.WebPages
 {
     public abstract class PageBase
     {
-        public Uri BaseUrl = new Uri("http://WIN-CV4AVBPAV8A/idsrv");
+        public Uri BaseUrl = new Uri(Settings.Default.TestEnvironment);
         
         public void Open()
         {
@@ -33,30 +33,6 @@ namespace IdmAutotests.WebPages
             Contract.Requires(url != null);
 
             Browser.Navigate(url);
-        }
-
-        protected static Uri GetUriByRelativePath(string relativePath)
-        {
-            return new Uri(string.Format("{0}{1}", Settings.Default.TestEnvironment, relativePath).ToLower());
-        }
-
-        private string RelativePath
-        {
-            get
-            {
-                const string rootNamespaceName = "Autotests.WebPages.Root";
-                var className = GetType().FullName;
-
-                Contract.Assume(className != null);
-
-                var path = className
-                    .Replace(rootNamespaceName, string.Empty)
-                    .Replace(".", "/");
-                
-                path = Regex.Replace(path, "/Index$", "");
-
-                return path;
-            }
         }
     }
 }
